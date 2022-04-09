10967d492e283ec22349f4217f8e1a199e5306c4
seojin <comcxx11@gmail.com>
2022-04-09 16:13:14 +0900

    Initial Commit

---
+++ App/AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppApp.swift
+//  App
+//
+//  Created by 홍서진 on 2022/04/09.
+//
+
+import SwiftUI
+
+@main
+struct AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  App
+//
+//  Created by 홍서진 on 2022/04/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
